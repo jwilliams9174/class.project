3589a197799ce4d97f13d47a38ae18f11f76907f
John Williams <john.williams-8@selu.edu>
2022-11-17 19:47:42 +0000

    Initial commit

---
+++ orangetoblue.R
@@ -0,0 +1,14 @@
+#' Compare two colors together and return the product
+#' 
+#' @param x Color_O
+#' @param y Color_B
+#' 
+#' @return The numerical product of x and y
+#' 
+#' @export
+
+
+blue_to_orange <-function(color_B){
+  color_O <- (color_B - 100) * 200/50
+  return(color_O)
+}
