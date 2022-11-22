e617a191ae26bc88f34f89cc0c10c9ef53f8f7cb
John Williams <john.williams-8@selu.edu>
2022-11-22 21:18:51 +0000

    add function

---
+++ R/boxplot.R
@@ -0,0 +1,10 @@
+#' Compare two colors together and return the product
+#' 
+#' @param x color
+#' @param y body_depth
+#' 
+#' @return The numerical product of x and y
+#' 
+#' @export
+#' 
+#' ggplot(data = crabs, mapping = aes(x = color, y = body_depth, color = sex)) + geomplot()
