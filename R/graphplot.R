43e88c7af6949ae8717706290fc9b86424d31539
jwilliams9174 <114522510+jwilliams9174@users.noreply.github.com>
2022-11-28 23:43:20 -0600

    Add files via upload

---
+++ R/graphplot.R
@@ -0,0 +1,12 @@
+#' Compare two colors together and return the product
+#' 
+#' @param x color
+#' @param y body_depth
+#' 
+#' @return The numerical product of x and y
+#' 
+#' @export
+#' 
+#' sex_comparison <-crabs %>% 
+#' group_by(sex, color)
+#' ggplot(data = sex_comparison, mapping = aes(x = color, y = body_depth, color = sex))+ geom_point()
